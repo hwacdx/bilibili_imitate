a607128cb1c645ac3aad6814701f96448db1357f
hwacdx <huangwei1603@gmail.com>
2016-08-20 18:28:11 +0800

    create project

---
+++ bilibili_imitate/bilibili_imitate/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  bilibili_imitate
+//
+//  Created by hw on 16/8/20.
+//  Copyright © 2016年 hwacdx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ bilibili_imitate/bilibili_imitate/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  bilibili_imitate
+//
+//  Created by hw on 16/8/20.
+//  Copyright © 2016年 hwacdx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
